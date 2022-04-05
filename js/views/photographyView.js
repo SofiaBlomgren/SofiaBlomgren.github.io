8943e5ea8a7d52d7d08976478bbf2ffe6a3f2ec4
Sofia <sofiablo@kth.se>
2022-04-05 21:36:08 +0200

    img width

--- js/views/photographyView.js
+++ js/views/photographyView.js
@@ -3,7 +3,7 @@ function PhotographyView(props){
         <div class="photography-view">
             <div class="flex-container">
                 <div class="flex-item pv-development">
-                    <img class="pv-img" src="/images/concert_dark.jpg"></img>
+                    <img src="/images/concert_dark.jpg"></img>
                     <div class="pv-title">
                         <h1>Photography Portfolio</h1>
                         <i class="fas fa-code fa-2x"></i>
