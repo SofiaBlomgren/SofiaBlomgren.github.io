0b75b315235833d3fc480b8e360c30e743784e7b
Sofia <sofiablo@kth.se>
2022-05-09 13:16:04 +0200

    About + Photo basics

--- js/views/aboutView.js
+++ js/views/aboutView.js
@@ -1,13 +1,39 @@
 function AboutView(props){
     return (
-        <div class="photography-view">
+        <div class="about-view">
             <div class="flex-container">
-                <div class="flex-item pv-development">
-                    <img class="pv-img" src="/images/Sofia2.png"></img>
-                    <div class="pv-title">
-                        <h1>About Page</h1>
-                        <i class="fas fa-code fa-2x"></i>
-                        <h3>is under construction</h3>
+                <div class="flex-row">
+                    <div class="flex-column">
+                        <div class="av-title">Hi, I'm Sofia!</div>
+                        <p>I'm an engineer and a photographer who grew up in Stockholm, Sweden. I've always had both a technical and a creative side. 
+                            Since I was a kid I've been interested in knowing how things work, and for as long as I can remember I've been fascinated by capturing the moments around me with a camera.</p>
+                        <p>After taking the natural sciences track in high school I decided that I also wanted to explore my more creative side, so in 2012 I moved to California to study photography and media at Santa Barbara City College. 
+                            I moved back to Stockholm in 2014, started my own photography business and began working on building a career as a freelance photographer. 
+                            Even though I was enjoying working with photography, I quickly realized that it wasn't what I wanted to do full time and in 2015 I started studying to become an engineer in media technology at KTH Royal Institute of Technology in Stockholm.</p>
+                        <p>Studying media technology was the perfect match for me, since it allowed me to mix my technical and my creative side. At KTH I got to explore a wide range of media technology, from using code to create everything from websites to VR games, to working with user eperience, user tests, personas and user journeys, to learning about the technology behind audio, video, photography and print. Aside from all of that, the most important thing I learned at KTH was how to efficiently find and solve problems in any situation.</p>
+                        <p>At KTH I was also allowed to work on other skills outside of the studies, mainly in the yearly reception of new students at my program. Two of the years I had bigger responsibilities within the reception, one year as a group leader and the year after that as project leader for the entire reception.
+                            Those years gave me a great understanding of how to work in groups and gave me hands on experience with how to take on a leadership role and how to plan and manage a big project.</p>
+                        <p>I've kept my photography business running throughout my studies at KTH, mainly shooting concerts for the photo agency Rockfoto where I'm also photo editor.
+                            I was also able to do photography at KTH, mainly for the student union paper Osqledaren which I was a part of for five years switching between the roles of photographer, media producer and photo editor.</p>
+                        <p>With my wide education in technology, combined with my creative side and photography skills, as well as my project management experience, I have a lot to offer both as a photographer and as a newly graduated engineer. Feel free to contact me if you want to talk or if you have any questions!</p>
+                        <div class="flex-row">
+                            <div class="av-contact">
+                                <div class="av-contact-title">Contact</div>
+                                <div>
+                                    <ul class="list-inline">
+                                    <li><b>Email (engineer): </b>sofiablo@kth.se</li>
+                                    <li><b>Email (photo): </b>sofiablomgrenphoto@gmail.com</li>
+                                    <li><b>Phone: </b>+46 (0)70-692 87 11</li>
+                                    <li><a href="/pdf/SofiaBlomgren_CV.pdf" target="_blank">View CV</a></li>
+                                    </ul>
+                                    <a href="https://www.linkedin.com/in/sofiablomgren/" target="_blank"><img class="av-icon" src="/images/icon_linkedin.png"></img></a>
+                                    <a href="https://www.instagram.com/sofiablomgrenphoto/" target="_blank"><img class="av-icon" src="/images/icon_instagram.png"></img></a>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex-column">
+                        <img class="av-img" src="/images/SofiaBlob.png"></img>
                     </div>
                 </div>
             </div>
--- js/views/photographyView.js
+++ js/views/photographyView.js
@@ -1,6 +1,179 @@
 function PhotographyView(props){
     return (
-        <div class="photography-view">
+        <div class="photography-view" oncontextmenu="return false;">
+        
+            <div class="container pv-tabs">
+
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-bs-toggle="tab" href="#tab-concerts">Concerts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#tab-weddings">Weddings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#tab-published">Published</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#tab-other">Other</a>
+                    </li>
+                </ul>
+
+                <div class="tab-content">
+                    <div class="tab-pane container tabContent active" id="tab-concerts">
+
+                        <div class="pv-row">
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/JacobBanks_2048px.jpg"></img>
+                                <img src="/images/photography/2048/HenrikBerggren_2048px.jpg"></img>
+                                <img src="/images/photography/2048/The1975_2048px.jpg"></img>
+                                <img src="/images/photography/2048/AliceCooper_2048px.jpg"></img>
+                                <img src="/images/photography/2048/BackyardBabies_2048px.jpg"></img> 
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/BMTH_2048px.jpg"></img>
+                                <img src="/images/photography/2048/LittleDragon_2048px.jpg"></img>
+                                <img src="/images/photography/2048/Bastille_2048px.jpg"></img>
+                                <img src="/images/photography/2048/CharliXCX_2048px.jpg"></img>
+                                <img src="/images/photography/2048/DuaLipa_2048px.jpg"></img>
+                                <img src="/images/photography/2048/Aurora_2048px.jpg"></img>
+                                <img src="/images/photography/2048/HAIM_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/FlorenceAndTheMachine_2048px.jpg"></img>
+                                <img src="/images/photography/2048/Janice_2048px.jpg"></img>
+                                <img src="/images/photography/2048/IggyPop_2048px.jpg"></img>
+                                <img src="/images/photography/2048/HollywoodVampires_2048px.jpg"></img>
+                                <img src="/images/photography/2048/Hurula_2048px.jpg"></img>
+                                <img src="/images/photography/2048/IconaPop_2048px.jpg"></img>
+                                <img src="/images/photography/2048/JamesBay_1_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/LennyKravitz_2048px.jpg"></img>
+                                <img src="/images/photography/2048/DanielAdamsRay_2048px.jpg"></img>
+                                <img src="/images/photography/2048/JamesBay_2_2048px.jpg"></img>
+                                <img src="/images/photography/2048/Amason_2048px.jpg"></img>
+                                <img src="/images/photography/2048/KaptenRod_2048px.jpg"></img>
+                                <img src="/images/photography/2048/Kent_2048px.jpg"></img>
+                                <img src="/images/photography/2048/HakanHellstrom_2048px.jpg"></img>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="tab-pane container tabContent" id="tab-concerts">
+
+                        <div class="pv-row">
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/JacobBanks_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/BMTH_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/FlorenceAndTheMachine_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/LennyKravitz_2048px.jpg"></img>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="tab-pane container tabContent" id="tab-weddings">
+
+                    <div class="pv-row">
+                            <div class="pv-column">
+                                <img src="/images/photography/weddings/wedding_ja_3_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_is_3_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_pc_6_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_6_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_is_4_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_8_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_cm_2_2048px.jpg"></img> 
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/weddings/wedding_ja_2_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_cm_5_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_pc_4_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_5_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_pc_1_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_12_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_pc_5_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/weddings/wedding_pc_2_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_cm_1_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_10_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_1_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_is_2_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_11_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_cm_3_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/weddings/wedding_pc_7_2048px.jpg"></img>
+                                
+                                <img src="/images/photography/weddings/wedding_ja_4_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_is_1_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_7_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_ja_9_2048px.jpg"></img>
+                                <img src="/images/photography/weddings/wedding_cm_6_2048px.jpg"></img>
+                                
+                                <img src="/images/photography/weddings/wedding_pc_8_2048px.jpg"></img>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="tab-pane container tabContent" id="tab-published">
+
+                    <div class="pv-row">
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/JacobBanks_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/BMTH_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/FlorenceAndTheMachine_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/LennyKravitz_2048px.jpg"></img>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="tab-pane container tabContent" id="tab-other">
+
+                    <div class="pv-row">
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/JacobBanks_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/BMTH_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/FlorenceAndTheMachine_2048px.jpg"></img>
+                            </div>
+                            <div class="pv-column">
+                                <img src="/images/photography/2048/LennyKravitz_2048px.jpg"></img>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+/*
+
             <div class="flex-container">
                 <div class="flex-item pv-development">
                     <img src="/images/concert_dark.jpg"></img>
@@ -11,6 +184,7 @@ function PhotographyView(props){
                     </div>
                 </div>
             </div>
-        </div>
-    )
-}
+
+
+
+*/
